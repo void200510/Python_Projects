fec67bb0b86718aad9a0e444dffbfb0d14644cd0
void200510 <147335164+void200510@users.noreply.github.com>
2024-12-14 12:04:00 +0530

    Update Number_Guessing_Game.py

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,6 +1,16 @@
 import random as r
 chances = 5
 
+print('''
+Welcome to the Number Guessing Game!
+Rules:
+1. Enter an +ve integer value only.
+2. Choose the maximum range (must be greater than 0).
+3. The random number will be between 1 and your chosen maximum range.
+4. Chances will not be deducted for out-of-range inputs.
+5. You have a total of 5 chances to guess the number.
+''')
+
 # taking input from user 
 def user_input(message):
     while True:
@@ -11,35 +21,41 @@ def user_input(message):
             print("please enter a interger value like 1,2,3,5 etc")
 
 def guessing(ran_num, maxrange, chance):
-    while chances > 0: # check if chances are left
+    while chance > 0: # check if chances are left
         user = user_input("Please guess a number: ") #taking the number from user that user gussed
         if user == ran_num: # if user hussed the number correctly
-            print("you beat the game!!!")
+            print("🎉you beat the game!!!")
             print("Thanks for playing")
             break
         # if user exceed the given range
-        elif user > maxrange:
-            print("please enter a number within range")
+        elif user > maxrange or user < 0:
+            print("⚠️please enter a number within range")
         else:
             # with wrong answer chance will be reduce till 0
             chance -= 1
 
             #checking if user gussed no. is too low or too high
             if user > ran_num:
-                print("the number is too big")
+                print("⚠️the number is too big")
             else:
-                print("the number is too small")
-            print("Chances left", chances) # remainding user of chances they have
+                print("⚠️the number is too small")
+            print("Chances left", chance) # remainding user of chances they have
     else:
         # if user has 0 chances then ending the game and declaring the answer
         print("Run out of chances")
-        print("The number is", ran_num)
+        print(f"😔The number is{ran_num}")
         print("Thanks for playing")
 
 def play_game():
     # limiting the random value range for game
-    max_range = user_input("please provide maximum range: ")
-    random_num = r.randrange(0, max_range) # generating the random number
+    while True:
+        max_range = user_input("please provide maximum range: ")
+        
+        if max_range <= 0:
+            print("please enter a value which is greater than 0")
+        else:
+            break
+    random_num = r.randint(1, max_range) # generating the random number
     guessing(random_num, max_range, chances)
 
 def main():
