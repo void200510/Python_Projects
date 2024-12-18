60fa85d6530abafae2ba529e224094ead4314e12
void200510 <147335164+void200510@users.noreply.github.com>
2024-12-18 18:57:59 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -6,24 +6,24 @@ def Input(message) -> int:
             return number
         except ValueError:
             print("please enter a valid input for example numbers")
-            continue
 
 #calculate factorial through logic (5! = 5 * 4!)
 def factorial(num) -> int:
-    if num in (0,1):
+    if num in (0, 1):
         return 1
-    else:
-        return num * factorial(num - 1)
+    return num * factorial(num - 1)
 
-#staring point of program asking user to continue or quit and the number that it want get factorial of.
+#starting point of program asking user to continue or quit and the number that it wants the factorial of.
 def starting():
     while True:
         user = input("Enter q for exiting the program or just simply press enter to continue the program: ").lower()
         if user == "q":
             print("Thank you for using it")
             break
-        else:
+        try:
             print(factorial(Input("Enter a number")))
+        except RecursionError:
+            print("Number too large to calculate factorial.")
 
 if __name__ == "__main__":
     starting()
